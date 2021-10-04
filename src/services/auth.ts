bd16757dfda7c0d3806b73fe6d549abacf17a781
Olivier Bitsch <olivier.b@iabsis.com>
2021-10-04 12:13:03 +0200

    Hash message before signature

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,14 +2,18 @@ import { CliqueClient, Wallet } from "alephium-js";
 import axios from "axios";
 import { config } from "../config/config";
 
+const EC = require('elliptic').ec;
+const Sha256 = require("crypto-js/sha256")
+
+const ec = new EC('secp256k1');
 const cliqueClient = new CliqueClient();
 
 export const authenticate = (wallet: Wallet) => {
-  const date = Date.now();
-  const signature = cliqueClient.messageSign(
-    date.toString(),
-    wallet.privateKey
-  );
+  const key = ec.keyFromPrivate(wallet.privateKey)
+  const date = Date.now().toString();
+  const msg = Sha256(date).toString()
+  console.log(msg);
+  var signature = Buffer.from(key.sign(msg).toDER()).toString('hex');
 
   const json = {
     publicKey: wallet.publicKey,
