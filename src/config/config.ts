3de578905b3ed8a4d24a85d2649b11a3fd043be1
Olivier Bitsch <olivier.b@iabsis.com>
2021-10-08 13:27:33 +0200

    New rebuild with new stripe key and sdk install

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: "https://alphmining.oniabsis.com",
   STRIPE_PUB_KEY:
-    "pk_test_51JcmarGXVGghJdQ9TR9DOng9HExpD7uiwpU2DGCqDGxnhJqYo6wH0A6dTT89K9VRAKCIUnopPpiqz2cUCfu7xl1E00tI5B8Vf2",
+    "pk_test_51JiGyYB16wIqKxGHPN64qYknmgiVQd4AGEe1aLw8GTyWZeVB6aR9gz23ZESRikiF7TrG8fLZZA7VhO8rsj04OZzV000SzUdEbW",
 };
