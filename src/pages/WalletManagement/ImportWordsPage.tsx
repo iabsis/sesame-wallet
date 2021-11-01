3d078ca4f0eb27ca527a9a0d44e4196451955b5e
Gilles <gilles.h@iabsis.com>
2021-11-01 10:30:07 +0100

    Added message for invalid seed import

--- src/pages/WalletManagement/ImportWordsPage.tsx
+++ src/pages/WalletManagement/ImportWordsPage.tsx
@@ -1,80 +1,72 @@
-import Tagify, { BaseTagData, ChangeEventData, TagData } from '@yaireo/tagify'
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import { TextAreaTags } from '../../components/Inputs'
-import {
-  FooterActions,
-  MainPanel,
-  PanelContainer,
-  PanelContent,
-  PanelTitle,
-  SectionContent
-} from '../../components/PageComponents'
-import { bip39Words } from '../../utils/bip39'
-import { Button } from '../../components/Buttons'
-import { StepsContext } from '../MultiStepsController'
-import { CenteredSecondaryParagraph } from '../../components/Paragraph'
-import { walletImport, getStorage } from 'alephium-js'
-import { GlobalContext } from '../../App'
-import { WalletManagementContext } from './WalletManagementContext'
-import { isHTTPError } from '../../utils/api'
+import Tagify, { BaseTagData, ChangeEventData, TagData } from "@yaireo/tagify";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { TextAreaTags } from "../../components/Inputs";
+import { FooterActions, MainPanel, PanelContainer, PanelContent, PanelTitle, SectionContent } from "../../components/PageComponents";
+import { bip39Words } from "../../utils/bip39";
+import { Button } from "../../components/Buttons";
+import { StepsContext } from "../MultiStepsController";
+import { CenteredSecondaryParagraph } from "../../components/Paragraph";
+import { walletImport, getStorage } from "alephium-js";
+import { GlobalContext } from "../../App";
+import { WalletManagementContext } from "./WalletManagementContext";
+import { isHTTPError } from "../../utils/api";
 
-const Storage = getStorage()
+const Storage = getStorage();
 
 const ImportWordsPage = () => {
-  const { setWallet, setSnackbarMessage } = useContext(GlobalContext)
-  const { password, username } = useContext(WalletManagementContext)
-  const { onButtonBack, onButtonNext } = useContext(StepsContext)
+  const { setWallet, setSnackbarMessage } = useContext(GlobalContext);
+  const { password, username } = useContext(WalletManagementContext);
+  const { onButtonBack, onButtonNext } = useContext(StepsContext);
 
-  const [phrase, setPhrase] = useState<{ value: string }[]>([])
-  const allowedWords = useRef(bip39Words.split(' '))
-  const [customPlaceholder, setCustomPlaceholder] = useState('Type your 24 words')
-  const tagifyRef = useRef<Tagify<TagData> | undefined>()
+  const [phrase, setPhrase] = useState<{ value: string }[]>([]);
+  const allowedWords = useRef(bip39Words.split(" "));
+  const [customPlaceholder, setCustomPlaceholder] = useState("Type your 24 words");
+  const tagifyRef = useRef<Tagify<TagData> | undefined>();
 
   const handlePhraseChange = (event: CustomEvent<ChangeEventData<BaseTagData>>) => {
     // Split words where spaces are
-    console.log("change phrase",  event.detail.value);
-    const newPhrase = event.detail.value && JSON.parse(event.detail.value)
-    setPhrase(newPhrase || [])
-    setCustomPlaceholder(
-      newPhrase.length > 0
-        ? newPhrase.length === 24
-          ? ''
-          : `${24 - newPhrase.length} words left`
-        : 'Type your 24 words'
-    )
-  }
+    console.log("change phrase", event.detail.value);
+    const newPhrase = event.detail.value && JSON.parse(event.detail.value);
+    setPhrase(newPhrase || []);
+    setCustomPlaceholder(newPhrase.length > 0 ? (newPhrase.length === 24 ? "" : `${24 - newPhrase.length} words left`) : "Type your 24 words");
+  };
 
   const isNextButtonActive = () => {
-    return phrase.length === 24
-  }
+    return phrase.length === 24;
+  };
 
   useEffect(() => {
     if (tagifyRef.current) {
-      tagifyRef.current.DOM.input.setAttribute('data-placeholder', customPlaceholder)
+      tagifyRef.current.DOM.input.setAttribute("data-placeholder", customPlaceholder);
     }
-  }, [customPlaceholder])
+  }, [customPlaceholder]);
 
   const handleWalletImport = () => {
     const formatedPhrase = phrase
       .map((w) => w.value)
       .toString()
-      .replace(/,/g, ' ')
+      .replace(/,/g, " ");
 
     try {
-      const wallet = walletImport(formatedPhrase)
+      const wallet = walletImport(formatedPhrase);
 
-      setWallet(wallet)
+      setWallet(wallet);
 
-      const encryptedWallet = wallet.encrypt(password)
-      Storage.save(username, encryptedWallet)
+      const encryptedWallet = wallet.encrypt(password);
+      Storage.save(username, encryptedWallet);
 
-      onButtonNext()
-    } catch (e) {
+      onButtonNext();
+    } catch (e: any) {
       if (isHTTPError(e)) {
-        setSnackbarMessage({ text: e.error.detail, type: 'alert' })
+        setSnackbarMessage({ text: e.error.detail, type: "alert" });
+      } else {
+        setSnackbarMessage({
+          text: e.message,
+          type: "alert",
+        });
       }
     }
-  }
+  };
 
   console.log(allowedWords);
   return (
@@ -83,17 +75,10 @@ const ImportWordsPage = () => {
         <PanelTitle color="primary">Secret words</PanelTitle>
         <PanelContent>
           <SectionContent>
-            <TextAreaTags
-              tagifyRef={tagifyRef}
-              placeholder={phrase.length.toString()}
-              whitelist={allowedWords.current}
-              onChange={handlePhraseChange}
-            />
+            <TextAreaTags tagifyRef={tagifyRef} placeholder={phrase.length.toString()} whitelist={allowedWords.current} onChange={handlePhraseChange} />
           </SectionContent>
           <CenteredSecondaryParagraph>
-            {!isNextButtonActive()
-              ? 'Make sure to properly write down the 24 secret words. They are the key to your wallet.'
-              : "All good? Let's continue!"}
+            {!isNextButtonActive() ? "Make sure to properly write down the 24 secret words. They are the key to your wallet." : "All good? Let's continue!"}
           </CenteredSecondaryParagraph>
         </PanelContent>
         {isNextButtonActive() && (
@@ -106,7 +91,7 @@ const ImportWordsPage = () => {
         )}
       </PanelContainer>
     </MainPanel>
-  )
-}
+  );
+};
 
-export default ImportWordsPage
+export default ImportWordsPage;
