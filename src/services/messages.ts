29c552bbf65029ca7564336cf00b109b15edacff
Gilles <gilles.h@iabsis.com>
2021-11-09 18:19:21 +0100

    Removed mocked data

--- src/services/messages.ts
+++ src/services/messages.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { config } from "../config/config";
-import { fakeResponse } from "./fakeResponse";
 
 export enum MessageType {
   low,
@@ -18,38 +17,7 @@ export type Message = {
   url: string | null;
 };
 
-const messages: Array<Message> = [
-  {
-    _id: "1",
-    title: "Un titre de message",
-    description: "lorem dslkfj dlksjf kdjfh kadjlshf kljadshf kjdshf kljs fkjlsd sdfh ksjda hfkjsdh fkjsadf aklsjgfhdshgfjgfjhhg adfjk hsdkjf dfsdfs fsdf sdf",
-    priority: MessageType.low,
-    date: new Date(),
-    expiry_date: new Date(),
-    url: null,
-  },
-  {
-    _id: "2",
-    title: "Un titre de message",
-    description: "lorem dslkfj dlksjf kdjfh kadjlshf kljadshf kjdshf kljs fkjlsd sdfh ksjda hfkjsdh fkjsadf aklsjgfhdshgfjgfjhhg adfjk hsdkjf dfsdfs fsdf sdf",
-    priority: MessageType.medium,
-    date: new Date(),
-    expiry_date: new Date(),
-    url: "http://iabsis.com",
-  },
-  {
-    _id: "3",
-    title: "Un titre de message",
-    description: "lorem dslkfj dlksjf kdjfh kadjlshf kljadshf kjdshf kljs fkjlsd sdfh ksjda hfkjsdh fkjsadf aklsjgfhdshgfjgfjhhg adfjk hsdkjf dfsdfs fsdf sdf",
-    priority: MessageType.high,
-    date: new Date(),
-    expiry_date: new Date(),
-    url: null,
-  },
-];
-
 export const getMessages = (jwtToken?: String): Promise<{ data: Array<Message> }> => {
-  // return fakeResponse(messages);
   return axios.get(`${config.API_URL}/messages`, {
     headers: {
       Authorization: jwtToken,
